00cfffb7446adb0153339db8b9f4f65c4a5efc81
Aljosa Makevic <aljosa100@gmail.com>
2025-02-05 17:03:19 +0400

    writing basic tests

---
+++ contracts/SimpleMessage.sol
@@ -0,0 +1,36 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+contract SimpleMessage {
+    string public message;
+    address public owner;
+
+    event MessageChanged(string indexed newMessage, address indexed changedBy);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    constructor(string memory initialMessage) {
+        if (bytes(initialMessage).length == 0) {
+            message = "Hey there AD";
+        } else {
+            message = initialMessage;
+        }
+    }
+
+    function setMessage(string memory _message) public onlyOwner {
+        message = _message;
+        emit MessageChanged(_message, msg.sender);
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "New owner cannot be zero address");
+        owner = newOwner;
+    }
+}
