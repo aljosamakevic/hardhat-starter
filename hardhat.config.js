a314c513fae9b3fc85d0d1948589e18aa2cf66b2
Aljosa Makevic <aljosa100@gmail.com>
2025-02-06 02:50:54 +0400

    init commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,20 +4,6 @@ import "dotenv/config";
 
 const config = {
   solidity: "0.8.28",
-  networks: {
-    "stavanger": {
-      url: "https://rpc.stavanger.gateway.fm",
-      chainId: 50591822,
-      accounts: [`0x${process.env.PRIVATE_KEY}`],
-      
-    },
-    "sepolia": {
-      url: "https://sepolia.infura.io/v3/2N9POOyrFRVSKiIB0fdnTNxKU7k",
-      chainId: 11155111,
-      accounts: [`0x${process.env.PRIVATE_KEY}`],
-      
-    },
-  }
 };
 
 export default config;
